4f189949169c59a603fa121f4e245c1f0ea7e48d
kizao <laozaki11263@gmail.com>
2019-11-01 21:31:42 +0900

    ヘッダー

--- components/header.vue
+++ components/header.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
-div
-  nav.navbar.navbar-expand-md.navbar-dark.fixed-top.bg-orange
-    a.navbar-brand(href="" color="white" style="font-weight: 900;") soloMGU
-    button.navbar-toggler(type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation")
-      span.navbar-toggler-icon
-    button(v-b-modal.settingModal style="margin-left: 90%;" class="btn btn-default")
-      img(src="~/assets/gear.png" style="width:30px;" )
-    settingModal
+b-navbar(toggleable='md', type='dark' class="bg-orange")
+  b-navbar-toggle(target='nav_collapse')
+  b-navbar-brand(href=''  style="font-weight: 900;") soloMGU
+  b-collapse#nav_collapse(is-nav='')
+    b-navbar-nav.ml-auto
+      b-nav-item-dropdown(right='')
+        // Using button-content slot
+        template(slot='button-content')
+          img(src="~/assets/gear.png" style="width:30px;" )
+        b-dropdown-item(v-b-modal.settingModal) 詳細設定
+        b-dropdown-item(href='#') 問い合わせ
+
+  settingModal
+
 </template>
 <script>
   import settingModal from '~/pages/settingModal.vue'
@@ -25,7 +31,10 @@ div
   }
 </script>
 <style lang="scss">
-  .bg-orange{
+  bg-orange{
     background-color: #FF9900;
+  }
+  .dropdown-toggle::after {
+    content: none;
   }
 </style>
