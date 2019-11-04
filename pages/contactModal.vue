0a852651a90c9e3ad34e7d41ebdc029e48370bb3
kizao <laozaki11263@gmail.com>
2019-11-04 15:08:59 +0900

    fix 問い合わせ機能追加

--- pages/contactModal.vue
+++ pages/contactModal.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
 div
-  form.form-horizontal(name="ask-question" method="post" data-netlify="true" data-netlify-honeypot="bot-field")
-    input(type="hidden" name="form-name" value="ask-question")
+  form.form-horizontal(name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field")
+    input(type="hidden" name="form-name" value="contact")
     .form-group
-      label.col-sm-3.control-label(for='name1') お名前
+      label.col-sm-3.control-label お名前
       .col-sm-6
         input.form-control(type='text', name='name' required)
     .form-group
-      label.col-sm-3.control-label(for='email') メールアドレス
+      label.col-sm-3.control-label メールアドレス
       .col-sm-6
-        input.form-control(type='text', name='email' required)
+        input.form-control(type='email', name='email' required)
     .form-group
-      label.col-sm-3.control-label(for='ask1') お問い合わせ内容
+      label.col-sm-3.control-label お問い合わせ内容
       .col-sm-6
         textarea.form-control(rows='7', name='message' required)
     .form-group
       label.col-sm-3
       .col-sm-9
-        button.btn.btn-primary(nuxt-link to="/") 送信する
+        button.btn.btn-primary(type="submit" nuxt-link to="/") 送信する
 </template>
